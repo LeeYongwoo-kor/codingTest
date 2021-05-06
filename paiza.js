f6bf3276d64e1ab4c8a7898cb1593b4c54802773
LeeYongwoo-kor <newlyo1205@naver.com>
2021-05-06 23:09:05 +0900

    Paiza A5-2 another case

--- paiza.js
+++ paiza.js
@@ -1,50 +1,62 @@
 // *********************** PAIZA ONLY ************************
 var lines = [];
 
-let date = 0;
-let init = 0;
-let limit = 0;
-let cases = 0;
-let dietFailArr = [];
+let frameCount = 0;
+let pinCount = 0;
+let throwCount = 0;
+
+let score = 0;
+let scoreBoard = [];
 
 const line = (line) => {
-  const splits = line.split(" ").map((item) => Number(item));
+  let splits = line.split(" ").map((item) => Number(item) || item);
   if (lines.length === 0) {
-    date = splits[0];
-    init = splits[1];
-    limit = splits[2];
+    frameCount = splits[0];
+    pinCount = splits[1];
+    throwCount = splits[2];
     lines.push(line);
   } else {
-    if (dietFailArr.length <= date) {
-      dietFailArr.push([splits[0] * -1, splits[1]]);
+    if (throwCount !== splits.length) return;
+    let gameCount = 0;
+    let round = 1;
+    splits = splits.map((item) => (item === "G" ? 0 : item));
+    for (let i = 0; i < splits; i++) {
+      gameCount++;
+      score += splits[i];
+      if (round === frameCount) {
+        if (score === pinCount) {
+          if (gameCount === 1) {
+          }
+        }
+      } else {
+        if (score === pinCount) {
+          scoreBoard.push(score);
+          if (gameCount === 1) {
+            score[round - 1] += splits[i + 1] + splits[i + 2];
+          } else {
+            score[round - 1] += splits[i + 1];
+          }
+          score = 0;
+          gameCount = 0;
+          round++;
+        }
+        if (gameCount === 2) {
+          scoreBoard.push(score);
+          score = 0;
+          gameCount = 0;
+          round++;
+        }
+      }
     }
   }
 };
 
 const close = () => {
-  for (let i = 0; i < Math.pow(2, date) - 1; i++) {
-    let currentWeight = init;
-    const casesArr = Array.from(i.toString(2).padStart(date, 0)).map((item) =>
-      Number(item)
-    );
-    for (let j = 0; j < date; j++) {
-      currentWeight += dietFailArr[j][casesArr[j]];
-      if (currentWeight > limit) break;
-      if (j === date - 1) cases++;
-    }
-  }
-  console.log(cases);
+  console.log(lines[1]);
 };
 
 window.onload = () => {
-  line("8 300 400");
-  line("9 39");
-  line("48 38");
-  line("21 10");
-  line("14 45");
-  line("32 20");
-  line("32 48");
-  line("9 7");
-  line("19 16");
+  line("15 5 24");
+  line("5 5 5 4 G 1 G 5 3 2 1 4 4 G G 1 5 5 5 2 1 5 3 1");
   close();
 };

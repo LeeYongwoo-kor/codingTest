ef9804aca3fe631e767aa55de9ece811e981b4de
LeeYongwoo-kor <newlyo1205@naver.com>
2021-05-25 22:45:02 +0900

    add A5-2(simple root)

--- paiza.js
+++ paiza.js
@@ -5,58 +5,52 @@ let frameCount = 0;
 let pinCount = 0;
 let throwCount = 0;
 
-let score = 0;
-let scoreBoard = [];
+let score = [];
 
 const line = (line) => {
-  let splits = line.split(" ").map((item) => Number(item) || item);
+  let splits = line
+    .split(" ")
+    .map((item) => Number(item) || (item === "G" ? 0 : item));
   if (lines.length === 0) {
     frameCount = splits[0];
     pinCount = splits[1];
     throwCount = splits[2];
     lines.push(line);
-  } else {
-    if (throwCount !== splits.length) return;
-    let gameCount = 0;
-    let round = 1;
-    splits = splits.map((item) => (item === "G" ? 0 : item));
-    for (let i = 0; i < splits; i++) {
-      gameCount++;
-      score += splits[i];
-      if (round === frameCount) {
-        if (score === pinCount) {
-          if (gameCount === 1) {
-          }
-        }
-      } else {
-        if (score === pinCount) {
-          scoreBoard.push(score);
-          if (gameCount === 1) {
-            score[round - 1] += splits[i + 1] + splits[i + 2];
-          } else {
-            score[round - 1] += splits[i + 1];
-          }
-          score = 0;
-          gameCount = 0;
-          round++;
-        }
-        if (gameCount === 2) {
-          scoreBoard.push(score);
-          score = 0;
-          gameCount = 0;
-          round++;
-        }
-      }
-    }
+  } else if (lines.length === 1) {
+    score = splits;
+    lines.push(score);
   }
 };
 
 const close = () => {
-  console.log(lines[1]);
+  let round = 0;
+  let cnt = 0;
+  const result = score.reduce((acc, curr, idx, scoreArr) => {
+    cnt++;
+    round += curr;
+    if (round === pinCount) {
+      round = 0;
+      if (cnt === 1) {
+        cnt = 0;
+        return (acc +=
+          curr + (scoreArr[idx + 1] || 0) + (scoreArr[idx + 2] || 0));
+      } else {
+        cnt = 0;
+        return (acc += curr + scoreArr[idx + 1]);
+      }
+    } else {
+      if (cnt === 2) {
+        cnt = 0;
+        round = 0;
+      }
+      return (acc += curr);
+    }
+  }, 0);
+  console.log(result);
 };
 
 window.onload = () => {
-  line("15 5 24");
-  line("5 5 5 4 G 1 G 5 3 2 1 4 4 G G 1 5 5 5 2 1 5 3 1");
+  line("10 10 18");
+  line("3 4 G 1 8 2 6 2 10 2 7 G 10 10 10 9 1 3");
   close();
 };
--- paiza/A/A5-2.js
+++ paiza/A/A5-2.js
@@ -12,115 +12,45 @@ let frameCount = 0;
 let pinCount = 0;
 let throwCount = 0;
 
-function Frame(scoreBoard = [], score = 0) {
-  this.scoreBoard = scoreBoard;
-  this.score = score;
-}
-
-Frame.prototype.strike = function () {
-  if (this.scoreBoard[0] === pinCount) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-Frame.prototype.spare = function () {
-  if (this.scoreBoard.length >= 3) {
-    // Last Frame
-    if (this.scoreBoard[1] === pinCount) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    if (this.scoreBoard[0] === pinCount) {
-      return false;
-    }
-    let sum = this.scoreBoard[0] + (this.scoreBoard[1] || 0);
-    if (sum === pinCount) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
-
-const frameDivision = (arr) => {
-  let tmp = [];
-  let arrCopy = arr.slice();
-  let len = arr.length;
-  let count = frameCount;
-
-  for (let i = 0; i < len; i++) {
-    if (count === 1) {
-      tmp.push(arr);
-      break;
-    }
-    if (arrCopy[i] === pinCount) {
-      tmp.push(arr.splice(0, 1));
-      count--;
-    } else {
-      tmp.push(arr.splice(0, 2));
-      count--;
-      i++;
-    }
-  }
-
-  return tmp;
-};
+let score = [];
 
 reader.on("line", (line) => {
-  const splits = line.split(" ").map((item) => Number(item) || item);
+  const splits = line
+    .split(" ")
+    .map((item) => Number(item) || (item === "G" ? 0 : item));
   if (lines.length === 0) {
     frameCount = splits[0];
     pinCount = splits[1];
     throwCount = splits[2];
     lines.push(line);
-  } else {
-    if (throwCount !== splits.length) return;
-    const throwArr = splits.map((value) => (value === "G" ? 0 : value));
-    const gameBoard = frameDivision(throwArr).reduce((acc, curr) => {
-      return [...(acc || []), new Frame(curr)];
-    }, []);
-
-    gameBoard.forEach((game, idx, board) => {
-      let score = game.scoreBoard.reduce((a, b) => a + b);
-      if (idx === frameCount - 1) {
-        // Last Frame
-        let bonusgame = game.scoreBoard[game.scoreBoard.length - 1];
-        if (game.strike() && game.spare()) {
-          score = game.scoreBoard[0] + game.scoreBoard[1] * 2 + bonusgame * 3;
-        } else if (game.strike()) {
-          score = game.scoreBoard[0] + game.scoreBoard[1] * 2 + bonusgame * 2;
-        } else if (game.spare()) {
-          score += bonusgame;
-        }
-      } else {
-        if (game.strike()) {
-          if (board[idx + 1].strike()) {
-            score +=
-              pinCount +
-              (board[idx + 2].scoreBoard[0] || board[idx + 1].scoreBoard[1]);
-          } else {
-            score +=
-              board[idx + 1].scoreBoard[0] + board[idx + 1].scoreBoard[1];
-          }
-        } else if (game.spare()) {
-          score += board[idx + 1].scoreBoard[0];
-        }
-      }
-      game.score = score;
-    });
-
-    let total = 0;
-    for (let board of gameBoard) {
-      total += board.score;
-    }
-
-    lines.push(total);
+  } else if (lines.length === 1) {
+    score = splits;
+    lines.push(score);
   }
 });
 reader.on("close", () => {
-  console.log(lines[1]);
+  let round = 0;
+  let cnt = 0;
+  const result = score.reduce((acc, curr, idx, scoreArr) => {
+    cnt++;
+    round += curr;
+    if (round === pinCount) {
+      round = 0;
+      if (cnt === 1) {
+        cnt = 0;
+        return (acc +=
+          curr + (scoreArr[idx + 1] || 0) + (scoreArr[idx + 2] || 0));
+      } else {
+        cnt = 0;
+        return (acc += curr + scoreArr[idx + 1]);
+      }
+    } else {
+      if (cnt === 2) {
+        cnt = 0;
+        round = 0;
+      }
+      return (acc += curr);
+    }
+  }, 0);
+  console.log(result);
 });
